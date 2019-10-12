c97eb09bf0cb968703ac39ae5646d59a5ef97820
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2019-10-13 00:37:37 +0900

    wip

---
+++ front/src/components/App.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { Waiting } from "./Waiting";
+import { Dashboard } from "./Dashboard";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+export const App = () => {
+  const Main = () => {
+    switch (location.hostname) {
+      case "localhost":
+        return <Dashboard />;
+      default:
+        return (
+          <Waiting
+            fqdn={location.hostname}
+            port={Number(location.port.length === 0 ? 80 : location.port)}
+          />
+        );
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Main />
+    </React.Fragment>
+  );
+};
